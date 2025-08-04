ca5944f1c3d0973418a0fef2b21bf4e5454a92e1
Aaron-Ccente <aronccente@gmail.com>
2025-08-04 12:03:22 -0500

    Rutas para el usuarios, optimizando los endpoints GET

--- routers/users.py
+++ routers/users.py
@@ -1,19 +1,17 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from db.database import SessionLocal
+from db.config import get_db
 from models import models
+from typing import Dict, Union
 
 router = APIRouter(prefix="/user",
                    tags=["user"],
                    responses={404: {"message": "No encontrado"}})
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 @router.get("/")
 async def get_items(db: Session = Depends(get_db)):
-    return db.query(models.Item).all()
+    return db.query(models.User).all()
+
+@router.get("/name")
+async def get_name() -> Dict[str, Union[str, str]]:
+    return {'message': 'Hello'}
