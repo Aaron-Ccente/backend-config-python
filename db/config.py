2ec25050427fe9b238ab72907bea879be956bfd3
Aaron-Ccente <aronccente@gmail.com>
2025-08-04 12:04:00 -0500

    Configuracion para la sesion en la base de datos como sesion local

---
+++ db/config.py
@@ -0,0 +1,10 @@
+
+# Configuracion para la sesion local de la base de datos
+from db.database import SessionLocal
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
